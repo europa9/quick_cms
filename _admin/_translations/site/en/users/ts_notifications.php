a20c61f7708572175028f230220ac4994740cf91
Sindre Andre <siditlef@gmail.com>
2020-02-12 11:34:08 +0100

    tr

--- _admin/_translations/site/en/users/ts_notifications.php
+++ _admin/_translations/site/en/users/ts_notifications.php
@@ -1,5 +1,6 @@
 <?php
 $l_notification = "Notification";
+$l_notifications = "Notifications";
 $l_delete = "Delete";
 $l_actions = "Actions";
 $l_date = "Date";
--- _admin/_translations/site/no/users/ts_notifications.php
+++ _admin/_translations/site/no/users/ts_notifications.php
@@ -3,4 +3,5 @@ $l_actions = "Handlinger";
 $l_date = "Dato";
 $l_delete = "Slett";
 $l_notification = "Varsler";
+$l_notifications = "Varsler";
 ?>
