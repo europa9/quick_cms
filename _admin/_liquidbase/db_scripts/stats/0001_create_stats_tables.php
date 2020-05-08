c42891e1eb38962834762f804820ad3cf27a728a
Sindre A <siditlef@gmail.com>
2020-05-08 07:46:52 +0200

    user

--- _admin/_liquidbase/db_scripts/stats/0001_create_stats_tables.php
+++ _admin/_liquidbase/db_scripts/stats/0001_create_stats_tables.php
@@ -159,33 +159,6 @@ else{
 
 
 
-// Stats :: User agents
-$query = "SELECT * FROM $t_stats_user_agents LIMIT 1";
-$result = mysqli_query($link, $query);
-
-if($result !== FALSE){
-}
-else{
-	mysqli_query($link, "CREATE TABLE $t_stats_user_agents(
-	   stats_user_agent_id INT NOT NULL AUTO_INCREMENT,
-	   PRIMARY KEY(stats_user_agent_id), 
-	   stats_user_agent_string VARCHAR(250),
-	   stats_user_agent_browser VARCHAR(250),
-	   stats_user_agent_browser_version VARCHAR(250),
-	   stats_user_agent_browser_icon VARCHAR(250),
-	   stats_user_agent_os VARCHAR(250),
-	   stats_user_agent_os_version VARCHAR(250),
-	   stats_user_agent_os_icon VARCHAR(250),
-	   stats_user_agent_bot VARCHAR(250),
-	   stats_user_agent_bot_icon VARCHAR(250),
-	   stats_user_agent_url VARCHAR(250),
-	   stats_user_agent_type VARCHAR(250),
-	   stats_user_agent_banned INT)")
-	   or die(mysqli_error($link));
-
-	// Type = unknown, desktop, mobile, bot
-}
-
 
 // Stats :: Bot ipblock
 $query = "SELECT * FROM $t_stats_bot_ipblock LIMIT 1";
